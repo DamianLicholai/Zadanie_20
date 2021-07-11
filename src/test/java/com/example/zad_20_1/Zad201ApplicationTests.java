b6a997c3e7fa239a47a3fb856b04e822bf6bd0ea
dlich <dlicholai@gmail.com>
2021-07-11 15:39:37 +0200

    create

---
+++ src/test/java/com/example/zad_20_1/Zad201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zad_20_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
